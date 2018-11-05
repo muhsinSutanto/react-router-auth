40394db7f0ed48a0afec11f36699757d6110a35e
muhsinSutanto <sutantomuhsin@gmail.com>
2018-11-05 11:04:37 +0700

    components

---
+++ src/components/Employees.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Employees extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            employees : []
+         }
+    }
+    render() { 
+        return ( 
+            <div>
+            </div>
+             );
+    }
+}
+ 
+export default Employees;
